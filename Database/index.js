786e97df2d1341b404dc2f31d7329e1de29873b1
shahnazalqassab <s.alqassab@gmail.com>
2025-06-30 13:57:10 +0300

    creating needed folders & files

---
+++ Database/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Successfully Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('connection error', error.message)
+    })
+
+    const DB = mongoose.connection
+
+    module.exports = DB
