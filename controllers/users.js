3622f266768b6bdf440570cc8d802ec58c4e92e2
shahnazalqassab <s.alqassab@gmail.com>
2025-07-01 00:32:38 +0300

    log in function

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
 const { User } = require('../Models')
+const middleware = require('../middleware')
 
 
 
 // GET ALL USERS
 const GetAll = async (req, res) => {
-
+    try {  
+        const users = await User.find({})
+        res.status(200).send(users)
+    }catch (error) {
+        console.error(error)
+        res.status(500).send({ status: 'Error', message: 'An error has occurred while fetching users' })
+    }
 }
+
+
 // CREATE USER
 const CreateUser = async (req, res) => {
     try {
@@ -29,19 +38,42 @@ const CreateUser = async (req, res) => {
                 }
         }
     } catch (error) {
-        console.error(error)
+        console.log(error)
         res.status(401).send({status: 'Error', message: 'An error has occurred while creating user' })
     }
 }
 
-// GET USER BY ID
-const GetUserById = async (req, res) => {
-
-}
 
 // LOGIN USER
 const Login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        let user = await User.findOne({ username })
 
+        if (!user) {
+            return res.status(401).send({ status: 'Error', message: 'Invalid username' })
+        }
+        let isValid = await middleware.verifyPassword(password, user.password)
+
+        if (isValid) {
+            let payload = {
+                id: user._id,
+                username: user.username,
+                name: user.name,
+                email: user.email,
+                user_role: user.user_role,
+                status: user.status
+            }
+            let token = await middleware.createToken(payload)
+            return res.status(200).send({ status: 'Success', message: 'Login successful', token, user: payload })
+        }
+        
+        res.status(402).send({ status: 'Error', message: 'Invalid password' })
+
+    } catch (error) {
+        console.log(error)
+        res.status(401).send({ status: 'Error', message: 'An error has occurred while logging in'})
+    }
 }
 
 // UPDATE USER
